8254c6b9c5049ccc8b5b5c21bf2c7730bb9a0d2f
yeine_darr <yeinedarr@gmail.com>
2020-07-12 16:35:33 +0300

    add formatting configs

--- src/pages/main/App.style.ts
+++ src/pages/main/App.style.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 const AppContainer = styled.div`
   text-align: center;
-`
+`;
 
 const AppLogo = styled.img`
   height: 40vmin;
   pointer-events: none;
-`
+`;
 
 const AppHeader = styled.header`
   background-color: #282c34;
@@ -18,15 +18,10 @@ const AppHeader = styled.header`
   justify-content: center;
   font-size: calc(10px + 2vmin);
   color: white;
-`
+`;
 
 const AppLink = styled.a`
   color: #61dafb;
-`
+`;
 
-export {
-  AppContainer,
-  AppHeader,
-  AppLink,
-  AppLogo
-}
+export { AppContainer, AppHeader, AppLink, AppLogo };
--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
-import {AppContainer, AppLink, AppLogo, AppHeader} from './App.style';
+import logo from 'assets/logo.svg';
+import { AppContainer, AppHeader, AppLink, AppLogo } from './App.style';
 
-function App() {
-  return (
-    <AppContainer>
-      <AppHeader>
-        <AppLogo src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <AppLink
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </AppHeader>
-    </AppContainer>
-  );
-}
+const App: () => JSX.Element = () => (
+  <AppContainer>
+    <AppHeader>
+      <AppLogo src={logo} alt='logo' />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <AppLink
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Learn React
+      </AppLink>
+    </AppHeader>
+  </AppContainer>
+);
 
-export {App};
+export { App };
